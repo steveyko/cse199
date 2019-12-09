f434f8fa7ba0207ec4222b0baa0e5b6ca5d6ad37
Steve Ko <stevko@buffalo.edu>
2019-12-09 15:35:36 -0500

    added percentage calculation

--- final-grades/src/main.rs
+++ final-grades/src/main.rs
@@ -9,10 +9,13 @@ use field::Field;
 use grade::Grade;
 use preprocessing::clean_up;
 use std::collections::HashMap;
+use std::env;
 use std::error::Error;
 use std::io::{self, Stdin};
 
 type Record = HashMap<String, String>;
+
+const NON_TOPHAT_TOTAL: f32 = 66.0;
 
 fn clean_up_headers(mut rdr: csv::Reader<Stdin>) -> Result<csv::Reader<Stdin>, Box<dyn Error>> {
     let mut v: Vec<String> = Vec::new();
@@ -36,6 +39,10 @@ fn assert_point_range(record: &Record) {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    assert!(args.len() == 2, "Please provide a TopHat total.");
+    let tophat_total = &args[1].parse::<f32>().unwrap();
+
     let mut rdr = clean_up_headers(csv::Reader::from_reader(io::stdin()))?;
 
     for result in rdr.deserialize() {
@@ -48,6 +55,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             .expect("Total doesn't exist.")
             .parse::<f32>()
             .unwrap_or(0.0);
+        let total = (total / (tophat_total + NON_TOPHAT_TOTAL) * 100.0).ceil();
+
         let attendance = record
             .get(Field::Attendance.as_str())
             .expect("Attendance doesn't exist.")
@@ -72,6 +81,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             total,
             attendance
         );
+        assert!(total < 100.0, "Total is more than 100%.");
     }
     Ok(())
 }
